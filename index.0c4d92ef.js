1f6bda95c37293a818d9e0c149a6a341025a7ebd
Ren <buher1994@gmail.com>
2024-09-04 21:54:00 +0200

    make build

---
+++ index.0c4d92ef.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.left=Math.round(e.clientWidth/2-t.offsetWidth/2)+"px",t.style.top=Math.round(e.clientHeight/2-t.offsetHeight/2)+"px";
+//# sourceMappingURL=index.0c4d92ef.js.map
